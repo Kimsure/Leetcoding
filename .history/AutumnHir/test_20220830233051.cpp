ff482010402db65ff0c86e913cafd72ff0626e42
Kimsure <54945340+Kimsure@users.noreply.github.com>
2022-08-30 23:31:29 +0800

    update

---
+++ .history/AutumnHir/test_20220830233051.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stddef.h>
+using namespace std;
+#pragma pack(4) // 
+struct  S
+{
+	int x;
+	char y;
+	int z;
+	double a;
+};
+int main()
+{
+    S s;
+	cout << offsetof(S, x) << endl; // 0
+	cout << offsetof(S, y) << endl; // 4
+	cout << offsetof(S, z) << endl; // 8
+	cout << offsetof(S, a) << endl; // 12
+    cout << sizeof(s) << endl;
+	return 0;
+}
---
+++ .history/AutumnHir/test_20220830233057.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stddef.h>
+using namespace std;
+#pragma pack(4) // 指定4字节对齐方式就可以了。
+
+struct  S
+{
+	int x;
+	char y;
+	int z;
+	double a;
+};
+int main()
+{
+    S s;
+	cout << offsetof(S, x) << endl; // 0
+	cout << offsetof(S, y) << endl; // 4
+	cout << offsetof(S, z) << endl; // 8
+	cout << offsetof(S, a) << endl; // 12
+    cout << sizeof(s) << endl;
+	return 0;
+}
