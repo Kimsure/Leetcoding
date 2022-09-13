f37724b952b3922b8362adb6d256beb9a2c60cdd
Kimsure <54945340+Kimsure@users.noreply.github.com>
2022-09-13 21:08:59 +0800

    0913

--- Basic_Algorithm/DP.cpp
+++ Basic_Algorithm/DP.cpp
@@ -128,10 +128,23 @@ class Solution3 {
 };
 
 /*
- * 二维DP
+ * 二维DP 62. 不同路径
  * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）
  * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish” ）问总共有多少条不同的路径
  */
+//
+class Solution {
+private:
+    int dfs(int i, int j, int m, int n) {
+        if (i > m || j > n) return 0; // 越界了
+        if (i == m && j == n) return 1; // 找到一种方法，相当于找到了叶子节点
+        return dfs(i + 1, j, m, n) + dfs(i, j + 1, m, n);
+    }
+public:
+    int uniquePaths(int m, int n) {
+        return dfs(1, 1, m, n);
+    }
+};
 class Solution4  {
     public:
         Solution4() {
--- Basic_Algorithm/test.cpp
+++ Basic_Algorithm/test.cpp
@@ -9,11 +9,40 @@
 # include<algorithm>
 using namespace std;
 
+// int main() {
+//     stack<char> stk1;
+//     stack<char> stk2;
+//     stk1.push('(');
+//     int a = stk1.top();
+//     cout << a << ' ' << stk1.top();
+//     return 0;
+// }
+
 int main() {
-    stack<char> stk1;
-    stack<char> stk2;
-    stk1.push('(');
-    int a = stk1.top();
-    cout << a << ' ' << stk1.top();
-    return 0;
+    int col = 0, row = 0, q = 0;
+    cin >> col >> row >> q;
+    cin.ignore();
+    char s[col][row];
+    for(int i = 0; i < col; i++) {
+    // cout<<"\n请输入第"<<i+1<<"个字符串："<<endl;
+        gets(s[i]);
+    }
+    for(int i = 0; i < col; i++) {
+    // cout<<"\n请输入第"<<i+1<<"个字符串："<<endl;
+        cout << s[i] << endl;
+    }
+    int a = 0, b = 0, c = 0;
+    int index = 0;
+    for (int i = 0; i < q; i++) {
+        cin >> a >> b >> c;
+        index = a % (row*col);
+        // for (int j = 0; j < index; j++) {
+
+        // }
+
+    }
 }
+
+
+
+
